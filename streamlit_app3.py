e722fa4908017e0d84891bc4ea12936e4e63a31f
Learnerr23 <100431219+Learnerr23@users.noreply.github.com>
2022-11-02 16:14:13 +0700

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.header('Hello world!')
 st.header('st.button')
 
-if st.button('Say hello'):
-     st.write('Why hello there')
+if st.button('Pilih File'):
+     st.write('File terpilih!')
 else:
      st.write('Goodbye')
